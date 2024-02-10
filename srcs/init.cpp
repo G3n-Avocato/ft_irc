ad7badd799034fac7ad4b1ad46b87bf13c903dce
Laura Massonet <lamasson@2F7.42angouleme.fr>
2024-02-10 20:29:05 +0100

    gsga

--- srcs/init.cpp
+++ srcs/init.cpp
@@ -6,7 +6,7 @@
 /*   By: lamasson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 23:25:47 by lamasson          #+#    #+#             */
-/*   Updated: 2024/02/10 01:55:06 by lamasson         ###   ########.fr       */
+/*   Updated: 2024/02/10 18:18:07 by lamasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ Server&	Server::operator=(const Server &rhs) {
 Server::~Server() {
 }
 
-void	Server::get_server_info() {
+void	Server::_get_server_info() {
 	int status;
 
 	memset(&this->_hints, 0, sizeof this->_hints);
@@ -40,14 +40,39 @@ void	Server::get_server_info() {
 	
 }
 
-void	Server::bind_socket_to_port() {
-	
+void	Server::_bind_socket_to_port() {
+	int yes = 1;
+
 	for (this->_cp_serv = this->_servinfo; this->_cp_serv != NULL; this->_cp_serv = this->_cp_serv->ai_next) {
 		
 		this->_fd_l = socket(this->_cp_serv->ai_family, this->_cp_serv->ai_socktype, this->_cp_serv->ai_protocol);
 		if (this->_fd_l < 0)
 			continue ;
-		if (setsockopt(this->_fd_l, ))
+		if (setsockopt(this->_fd_l, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit (1);
+		}
+		if (bind(this->_fd_l, this->_cp_serv->ai_addr, this->_cp_serv->ai_addrlen) < 0) {
+			close(this->_fd_l);
+			continue ;
+		}
 	}
+	if (this->_cp_serv == NULL) {
+		fprintf(stderr, "serverinfo: failed to bind\n");
+		exit (2);
+	}
+}
+
+void	Server::() {
+	FD_ZERO(&this->_main);
+	FD_ZERO(&this->_tmp);
+
+	if (listen(this->_fd_l, 10) == -1) {
+		perror("listen");
+		exit (3);
+	}
+
+	FD_SET(this->_fd_l, &this->_main);
+	this->_fdmax = this->_fd_l;
 
 }
--- srcs/server.hpp
+++ srcs/server.hpp
@@ -6,7 +6,7 @@
 /*   By: lamasson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 23:21:00 by lamasson          #+#    #+#             */
-/*   Updated: 2024/02/10 01:38:23 by lamasson         ###   ########.fr       */
+/*   Updated: 2024/02/10 18:15:32 by lamasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #include <cstring>
 #include <cstdio>
 #include <cstdlib>
+#include <unistd.h>
 
 class Server {
 	
@@ -35,6 +36,16 @@ class Server {
 		struct addrinfo	*_servinfo;
 		const char		*_port;
 
+		void			_get_server_info();
+
 		int 			_fd_l;
 		struct addrinfo *_cp_serv;
+
+		void			_bind_socket_to_port();
+
+		fd_set			_main;
+		fd_set			_tmp;
+		int				_fdmax;
+
+		
 };
